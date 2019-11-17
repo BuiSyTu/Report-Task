c9bdc31ba647054e7a7bc323ec5ab45b19be1ce1
Bui Sy Tu <tubs@tandan.com.vn>
2019-11-17 21:41:53 +0700

    heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var pg = require("pg")
+
+var config = {
+  connectionString: 'postgres://xbziegmhxcxlas:7ce98c2cd9c14199200b76e62e9f8592d717f3e7a1c210f2a85947118130a41c@ec2-174-129-240-67.compute-1.amazonaws.com:5432/d3pkij1joc4ck0'
+}
+
+var pool = new pg.Pool(config)
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  pool.connect(function (err, client, done) {
+    if (err) {
+      return console.error('error ferching client from pool', err)
+    }
+    client.query('SELECT * from fruit.fruit', function (err, result) {
+      done();
+      if (err) {
+        res.end();
+        return console.error('error running query', err)
+      }
+      res.json(result.rows)
+    })
+  })
 });
 
 module.exports = router;
