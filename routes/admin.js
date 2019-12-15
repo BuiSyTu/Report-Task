530dd200aa01dec3fddd4513d9c0e592dddc1a03
Quang <duongquang16111997@gmail.com>
2019-12-15 16:35:56 +0700

    info user

--- routes/admin.js
+++ routes/admin.js
@@ -5,34 +5,37 @@ require("body-parser");
 var moment = require('moment');
 const uuid = require('uuid/v1');
 const axios = require('axios');
-
+const checkRole = require('../helper/checkRole')
 
 router.get('/report-list', (req, res, next) => {
-    
-    reportTask.getAllReportTask()
-        .then(report => {
-            for (i = 0; i < report.rows.length; i++) {
-                report.rows[i].content = JSON.parse(report.rows[i].content)
-            }
-            res.json(report.rows);
-            
-            
-        }).catch(() => {
-            
-            
-            res.json({ "status_code": "500" })
-        })
+
+  reportTask.getAllReportTask()
+    .then(report => {
+      for (i = 0; i < report.rows.length; i++) {
+        report.rows[i].content = JSON.parse(report.rows[i].content)
+      }
+      res.json(report.rows);
+
+
+    }).catch(() => {
+
+
+      res.json({ "status_code": "500" })
+    })
 })
 
 router.get('/report/:id', function (req, res, next) {
-    let id = req.params.id
-  
-    reportTask.getReportTaskById(id)
-      .then(report => {
-        res.json(JSON.stringify(report.rows[0]));
-      }).catch(() => {
-        res.json({ "status_code": "500" })
-      })
-  })
+  let id = req.params.id
 
+  reportTask.getReportTaskById(id)
+    .then(report => {
+      res.json(JSON.stringify(report.rows[0]));
+    }).catch(() => {
+      res.json({ "status_code": "500" })
+    })
+})
+
+router.get('/info', [checkRole.hasUserId], (req, res) => {
+  res.json(req.session.infoUser);
+})
 module.exports = router;
